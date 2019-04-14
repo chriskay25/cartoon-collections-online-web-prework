0018a484b9a132f8329c988ecc47a5dc72f16058
Chris Kay <chriskay25@gmail.com>
2019-04-14 13:42:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, ind|
-    puts 
-  # Your code here
+    puts "#{ind} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
